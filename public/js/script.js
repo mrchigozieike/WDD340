90b9dd40d0e17c16943d66513fcd2407e593c41c
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-06-23 03:25:17 +0100

    week2

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+const mainnav = document.querySelector('.nav')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});const visitsDisplay = document.querySelector("#visits");
