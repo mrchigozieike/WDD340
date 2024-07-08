44eae2f5e2ce0c885add2a19d8149d2cc2959969
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-07-08 14:12:16 +0100

    week 3 finished

--- utilities/index.js
+++ utilities/index.js
@@ -44,7 +44,7 @@ Util.buildClassificationGrid = async function (data) {
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => {
       grid += '<li>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id       
         + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model
         + 'details"><img src="' + vehicle.inv_thumbnail
         + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model
