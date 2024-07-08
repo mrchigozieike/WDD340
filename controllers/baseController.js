83c7c5a8e16cb1289dc660a081a5b83156efeecf
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-07-08 17:29:10 +0100

    massaging done

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -5,6 +5,7 @@ baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
   
   res.render("index", {title: "Home", nav})
+  req.flash("notice", "This is a flash message.")
 }
 
 module.exports = baseController
