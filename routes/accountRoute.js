e797e39871a054ed4752b32751159f23aa89dbd1
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-07-19 12:38:26 +0100

    progress week 5

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,13 +5,6 @@ const accountController = require("../controllers/accountController") // Brings 
 const Util = require('../utilities/')
 const regValidate = require('../utilities/account-validation')
 
-
-
-/* ***************************
- *  Build Account
- * ************************** */
-// Deliver Login View
-router.get("/login", Util.handleErrors(accountController.buildLogin))
 // Route to build Default Account View
 router.get("/register", Util.handleErrors(accountController.buildRegister))
 // Route to check Login
@@ -19,11 +12,6 @@ router.get("/", Util.checkLogin, Util.handleErrors(accountController.buildManage
 
 router.get('/logout', Util.handleErrors(accountController.accountLogout))
 router.get("/edit", Util.checkLogin, Util.handleErrors(accountController.buildAccountManagement))
-
-
-/* ****************************************
-*  Deliver login view
-* *************************************** */
 
 //Process the registration data
 router.post(
